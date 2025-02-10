3e89a2a1b15dd757e71f59d5ba5b9c1f1a2103da
902MACKY <keiganmacneil8@gmail.com>
2025-02-10 11:53:10 -0400

    Pause Menu cpp&h formula

---
+++ Framework-2025/SDL_Template/PauseMenu.h
@@ -0,0 +1,33 @@
+#ifndef __PAUSEMENU_H
+#define __PAUSEMENU_H
+
+#include "GameEntity.h"
+#include "Texture.h"
+#include "InputManager.h"
+
+using namespace SDLFramework;
+
+class PauseMenu : public GameEntity {
+private:
+    InputManager* mInput;
+
+    Texture* mBackgroundBox;
+    Texture* mResumeText;
+    Texture* mExitText;
+    Texture* mCursor;
+
+    Vector2 mCursorStartPos;
+    Vector2 mCursorOffset;
+    int mSelectedOption;
+
+public:
+    PauseMenu();
+    ~PauseMenu();
+
+    void ChangeOption(int change);
+    int SelectedOption();
+    void Update();
+    void Render();
+};
+
+#endif
