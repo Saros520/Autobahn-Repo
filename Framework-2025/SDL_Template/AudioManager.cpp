2c7d26eac89e0c5411459bb5a090ab1b7b06fab4
Joshua Goulden <joshtgoulden2000@gmail.com>
2025-03-12 23:11:55 -0300

    Trying to fix up stuff
    Trying to figure out if unregistering the entities with regards to collisions maybe contributing to the crash.

--- Framework-2025/SDL_Template/AudioManager.cpp
+++ Framework-2025/SDL_Template/AudioManager.cpp
@@ -2,71 +2,78 @@
 
 namespace SDLFramework {
 
-	AudioManager * AudioManager::sInstance = nullptr;
+    AudioManager* AudioManager::sInstance = nullptr;
 
-	AudioManager * AudioManager::Instance() {
-		if (sInstance == nullptr) {
-			sInstance = new AudioManager();
-		}
-		return sInstance;
-	}
+    AudioManager* AudioManager::Instance() {
+        if (sInstance == nullptr) {
+            sInstance = new AudioManager();
+        }
+        return sInstance;
+    }
 
-	void AudioManager::Release() {
-		delete sInstance;
-		sInstance = nullptr;
-	}
+    void AudioManager::Release() {
+        delete sInstance;
+        sInstance = nullptr;
+    }
 
-	void AudioManager::PlayMusic(std::string filename, int loops) {
-		Mix_PlayMusic(mAssetManager->GetMusic(filename), loops);
-	}
+    void AudioManager::PlayMusic(std::string filename, int loops) {
+        Mix_PlayMusic(mAssetManager->GetMusic(filename), loops);
+    }
 
-	void AudioManager::PlayMusic(Mix_Music * music, int loops) {
-		Mix_PlayMusic(music, loops);
-	}
+    void AudioManager::PlayMusic(Mix_Music* music, int loops) {
+        Mix_PlayMusic(music, loops);
+    }
 
-	void AudioManager::PauseMusic() {
-		if (Mix_PlayingMusic() != 0) {
-			Mix_PauseMusic();
-		}
-	}
+    void AudioManager::PauseMusic() {
+        if (Mix_PlayingMusic() != 0) {
+            Mix_PauseMusic();
+        }
+    }
 
-	void AudioManager::ResumeMusic() {
-		if (Mix_PausedMusic() != 0) {
-			Mix_ResumeMusic();
-		}
-	}
+    void AudioManager::ResumeMusic() {
+        if (Mix_PausedMusic() != 0) {
+            Mix_ResumeMusic();
+        }
+    }
 
-	void AudioManager::SetMusicVolume(int volume) {
+    void AudioManager::SetMusicVolume(int volume) {
+        std::cout << volume << std::endl;
+        Mix_VolumeMusic(volume);
+    }
 
-		std::cout << volume << std::endl;
+    void AudioManager::PlaySFX(std::string filename, int loops, int channel) {
+        Mix_Chunk* sfx = mAssetManager->GetSFX(filename);
+        if (sfx == nullptr) {
+            std::cerr << "Unable to load SFX " << filename << "! Mix error: " << Mix_GetError() << std::endl;
+            return;
+        }
+        Mix_PlayChannel(channel, sfx, loops);
+    }
 
-		Mix_VolumeMusic(volume);
-	}
+    void AudioManager::PlaySFX(Mix_Chunk* sfx, int loops, int channel) {
+        if (sfx == nullptr) {
+            std::cerr << "Mix_Chunk is null! Unable to play SFX." << std::endl;
+            return;
+        }
+        Mix_PlayChannel(channel, sfx, loops);
+    }
 
-	void AudioManager::PlaySFX(std::string filename, int loops, int channel) {
-		Mix_PlayChannel(channel, mAssetManager->GetSFX(filename), loops);
-	}
+    AudioManager::AudioManager() {
+        mAssetManager = AssetManager::Instance();
 
-	void AudioManager::PlaySFX(Mix_Chunk * sfx, int loops, int channel) {
-		Mix_PlayChannel(channel, sfx, loops);
-	}
+        if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0) {
+            std::cerr << "Unable to initialize SDL audio! SDL Error: " << SDL_GetError() << std::endl;
+            return;
+        }
 
-	AudioManager::AudioManager() {
-		mAssetManager = AssetManager::Instance();
+        if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0) {
+            std::cerr << "Unable to initialize audio! Mix Error: " << Mix_GetError() << std::endl;
+        }
+    }
 
-		if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0) {
-			std::cerr << "Unable to initialize SDL audio! SDL Error: " << SDL_GetError() << std::endl;
-			return;
-		}
-
-		if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0) {
-			std::cerr << "Unable to initialize audio! Mix Error: " << Mix_GetError() << std::endl;
-		}
-	}
-
-	AudioManager::~AudioManager() {
-		mAssetManager = nullptr;
-		Mix_CloseAudio();
-		Mix_Quit();
-	}
+    AudioManager::~AudioManager() {
+        mAssetManager = nullptr;
+        Mix_CloseAudio();
+        Mix_Quit();
+    }
 }
--- Framework-2025/SDL_Template/PhysicsManager.cpp
+++ Framework-2025/SDL_Template/PhysicsManager.cpp
@@ -33,6 +33,7 @@ void PhysicsManager::UnregisterEntity(unsigned long id) {
         for (int j = 0; j < mCollisionLayers[i].size() && !found; j++) {
             if (mCollisionLayers[i][j]->GetId() == id) {
                 mCollisionLayers[i].erase(mCollisionLayers[i].begin() + j);
+                std::cout << "Unregistered entity with ID: " << id << " from layer: " << i << std::endl;
                 found = true;
             }
         }
@@ -44,16 +45,19 @@ void PhysicsManager::Update() {
         for (unsigned int j = 0; j < static_cast<unsigned int>(CollisionLayers::MaxLayers); j++) {
             if (mLayerMasks[i].test(j) && i <= j) {
                 for (unsigned int k = 0; k < mCollisionLayers[i].size(); k++) {
+                    if (mCollisionLayers[i][k] == nullptr) {
+                        std::cout << "Null pointer detected in collision layer " << i << " at index " << k << std::endl;
+                        continue;
+                    }
                     for (unsigned int l = 0; l < mCollisionLayers[j].size(); l++) {
-                        if (mCollisionLayers[i][k] && mCollisionLayers[j][l]) {
-                            if (mCollisionLayers[i][k]->CheckCollision(mCollisionLayers[j][l])) {
-                                std::cout << "Collision detected between entities " << mCollisionLayers[i][k]->GetId() << " and " << mCollisionLayers[j][l]->GetId() << std::endl;
-                                mCollisionLayers[i][k]->Hit(mCollisionLayers[j][l]);
-                                mCollisionLayers[j][l]->Hit(mCollisionLayers[i][k]);
-                            }
+                        if (mCollisionLayers[j][l] == nullptr) {
+                            std::cout << "Null pointer detected in collision layer " << j << " at index " << l << std::endl;
+                            continue;
                         }
-                        else {
-                            std::cout << "Null pointer detected in collision layers at indices [" << i << "][" << k << "] or [" << j << "][" << l << "]" << std::endl;
+                        if (mCollisionLayers[i][k]->CheckCollision(mCollisionLayers[j][l])) {
+                            std::cout << "Collision detected between entities " << mCollisionLayers[i][k]->GetId() << " and " << mCollisionLayers[j][l]->GetId() << std::endl;
+                            mCollisionLayers[i][k]->Hit(mCollisionLayers[j][l]);
+                            mCollisionLayers[j][l]->Hit(mCollisionLayers[i][k]);
                         }
                     }
                 }
